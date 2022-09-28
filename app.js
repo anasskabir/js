ae8c19bee2aaddcc221acea312c45b28267efae2
Anass <anasskabir@gmail.com>
2022-09-28 17:25:25 -0500

    more js practice

--- app.js
+++ app.js
@@ -1,54 +1,78 @@
-
-// // 1. Then
-// getSubscriptionStatus().then(response => console.log(response))
-
-// 2. Async/Await
-// async function main() {
-    //     const status = (await getSubscriptionStatus())
-    //     statusRef.innerHTML = status
-    
-    // }
-    
-    // main();
-    
-
-/** 1. Creat a function called `Get VIdeo`
- *  2. Accept a parameter called `subscriptionStatus`
- *  3. Return a new Promise inside of the function that:
- *      - if "VIP" resolve ("show video")
- *      - if "FREE" resolve ("show trailer")
- *      - otherwise reject ("no video")
- *  4. console.log the result of getVideo() in main()
- */
-
-const statusRef = document.querySelector('.status')
-
-function getSubscriptionStatus () {
-    return new Promise ((resolve,reject) => {
-        setTimeout(() => {
-            resolve("VIP");
-    }, 2000);
-});
+// Addition
+function addition (num1, num2) {
+    return num1 + num2 ; 
 }
+console.log(addition(3,5))
 
-function getVideo (SubscriptionStatus) {
-    return new Promise ((resolve, reject) => {
-        if (SubscriptionStatus === 'VIP') {
-            resolve("show video")
-        }
-        else if (SubscriptionStatus === `FREE`) {
-            resolve (`Show Trailer`)
-        }
-        else {
-            reject (`no video`)
-        }
-    })
+// Hours into seconds
+function multiplication (hours) {
+    return hours * 3600
 }
+console.log (multiplication(2))
 
-async function main() {
-    const status = await getSubscriptionStatus();
-    statusRef.innerHTML = status
-    console.log(getVideo(status))
+// perimeter of a square/rectangle
+function calculatePerimeter(length, width){
+    return (length * 2) + (width * 2)
 }
+console.log(calculatePerimeter(6,7))
 
-main();
+// area of a triangle
+function areaTriangle (base,height) {
+    return .5*(base*height)
+}
+console.log(areaTriangle(10,10))
+
+// Extend a string
+function addFrontend (string) {
+    return string + `Frontend`
+}
+console.log(addFrontend(`apple`))
+
+// greater than 100
+function grtThan100 (num1, num2) {
+    return num1 + num2 > 100
+}
+console.log(grtThan100(50,70))
+
+// less than or equal to 0
+function zero (number) {
+    return number <= 0
+} 
+console.log(zero(6))
+
+//opposite Boolean
+function oppBoolean (bool){
+    return !bool
+}
+console.log(oppBoolean(true))
+
+// not the number 0
+function notZero (number) {
+    return number !== 0
+}
+console.log(notZero(0))
+
+//calculate the remainder
+function calcRemainder (num1, num2) {
+    return num1 % num2
+}
+console.log(calcRemainder(7,8))
+
+// is the number odd?
+function oddOrEven (num) {
+    return num % 2 !== 0
+}
+console.log(oddOrEven())
+
+//if even, return -1
+function ifEven (num) {
+    return num % 2 === 0? 1 : -1
+}
+console.log(ifEven(5))
+
+//check if user is logged in and subscribed
+function userStatus (string1, string2) {
+    return string1 === `Logged_in` && string2 === `subscribed`
+}
+console.log(userStatus(`Logged_in`, `subscribed`))
+
